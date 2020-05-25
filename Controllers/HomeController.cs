9164f26bba512f72361293710c8320c73893c46a
Miguel Angel Guerra Guerrero <miguel.guerra@agilethought.com>
2020-05-24 23:01:42 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DockersTraining_Compose_miguel_guerra.Models;
+using Microsoft.Extensions.Options;
+
+namespace DockersTraining_Compose_miguel_guerra.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly AppSettings _appSettings;
+
+ 
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.storename = _appSettings.storename;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockersTraining_Compose_miguel_guerra.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
